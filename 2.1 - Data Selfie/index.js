50809f79d6211782bb103afec0e5f0155531b17b
srz2 <srz222@gmail.com>
2019-11-12 02:57:40 -0500

    Added database functionality

--- 2.1 - Data Selfie/index.js
+++ 2.1 - Data Selfie/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 app.listen(3000, () => {
    console.log('Listening....'); 
 });
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 app.use(express.static('public'));
 app.use(express.json());
 
 app.post('/api', (req, res) => {
-    console.log(req.body);
+    const data = req.body;
+    data.timestamp = Date.now();
+    console.log(data);
+    database.insert(data);
     res.json({
-        status: "Success"
+        status: "Success",
+        data: data
     });
 });
